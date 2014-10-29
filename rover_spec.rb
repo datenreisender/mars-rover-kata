2714e08e44659945002ceace1b75131b63934b1a
Marko Schulz <marko.schulz@it-agile.de>
2014-10-29 02:03:53 +0100

    Clean up tests

--- rover_spec.rb
+++ rover_spec.rb
@@ -22,25 +22,24 @@ class Rover
 end
 
 describe Rover do
+  before(:example) do
+    @rover = Rover.new(1,1,:north)
+  end
+
   it 'can move around' do
-    rover = Rover.new(0,0,:north).move('ffrff')
-    expect(rover.position).to eq [2,2]
+    expect(@rover.move('ffrff').position).to eq [3,3]
   end
 
   it 'can move backward' do
-    rover = Rover.new(1,1,:north).move('b')
-    expect(rover.position).to eq [0,1]
+    expect(@rover.move('b').position).to eq [0,1]
   end
 
   it 'can move left and right' do
-    rover = Rover.new(1,1,:north).move('rflf')
-    expect(rover.position).to eq [2,2]
+    expect(@rover.move('rflf').position).to eq [2,2]
   end
 
   it 'can move in every direction' do
-    rover = Rover.new(1,1,:north).move('rfrfrfrf')
-    expect(rover.position).to eq [1,1]
-    rover = Rover.new(1,1,:north).move('blblblb')
-    expect(rover.position).to eq [1,1]
+    expect(@rover.move('rfrfrfrf').position).to eq [1,1]
+    expect(@rover.move('blblblbl').position).to eq [1,1]
   end
 end
