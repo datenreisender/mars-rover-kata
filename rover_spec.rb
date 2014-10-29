6c5c05c6603010cec516d37d492bf65ac3d86e05
Marko Schulz <marko.schulz@it-agile.de>
2014-10-29 01:15:26 +0100

    Kick it off with an acceptance test and a stubbed solution.
    Intention: Create fast a basic structure that I can adept and flesh out later.

--- rover_spec.rb
+++ rover_spec.rb
@@ -1,5 +1,19 @@
 class Rover
+  def initialize(x, y, direction)
+  end
+
+  def move(moves)
+    self
+  end
+
+  def position
+    [2,2]
+  end
 end
 
 describe Rover do
+  it 'can move around' do
+    rover = Rover.new(0,0,:north).move('ffrff')
+    expect(rover.position).to eq [2,2]
+  end
 end
