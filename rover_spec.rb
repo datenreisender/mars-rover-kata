3211504eea3406f2d9cf521fecc5f56161970d91
Marko Schulz <marko.schulz@it-agile.de>
2014-10-30 02:27:03 +0100

    Cleanup: Stronger test names

--- rover_spec.rb
+++ rover_spec.rb
@@ -5,24 +5,24 @@ describe Rover do
     @rover = Rover.new(1,1,:north)
   end
 
-  it 'can move around' do
+  it 'moves around' do
     expect(@rover.move('ffrff').position).to eq [3,3]
   end
 
-  it 'can move backward' do
+  it 'moves backward' do
     expect(@rover.move('b').position).to eq [0,1]
   end
 
-  it 'can move left and right' do
+  it 'turns left and right' do
     expect(@rover.move('rflf').position).to eq [2,2]
   end
 
-  it 'can move in every direction' do
+  it 'moves in any direction' do
     expect(@rover.move('rfrfrfrf').position).to eq [1,1]
     expect(@rover.move('blblblbl').position).to eq [1,1]
   end
 
-  it 'can start off in every direction' do
+  it 'starts in any direction' do
     expect(Rover.new(1,1,:north).move('f').position).to eq [2,1]
     expect(Rover.new(1,1,:east ).move('f').position).to eq [1,2]
     expect(Rover.new(1,1,:south).move('f').position).to eq [0,1]
